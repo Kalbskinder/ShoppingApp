b88884f39c48061bb6bbf86a16002434a63cd5f3
Kalbskinder <mimosc007@gmail.com>
2025-09-02 09:09:24 +0200

    Added category detection array

--- shoppingapp/src/app/data.ts
+++ shoppingapp/src/app/data.ts
@@ -1,7 +1,23 @@
+// Automaticly select a category based on the item name
+// When no category is found, "Other" is used
 export const shoppingData = [
-    { name: "Banana", category: "Fruits" }
-]
+  { name: "Banana", category: "Fruits" },
+  { name: "Apple", category: "Fruits" },
+  { name: "Carrot", category: "Vegetables" },
+  { name: "Broccoli", category: "Vegetables" },
+  { name: "Chocolate Bar", category: "Sweets & Snacks" },
+  { name: "Potato Chips", category: "Sweets & Snacks" },
+  { name: "Shampoo", category: "Hygiene" },
+  { name: "Toothpaste", category: "Hygiene" },
+  { name: "Orange Juice", category: "Beverages" },
+  { name: "Mineral Water", category: "Beverages" },
+  { name: "Frozen Pizza", category: "Frozen" },
+  { name: "Ice Cream", category: "Frozen" },
+  { name: "Chicken Breast", category: "Meat" },
+  { name: "Ground Beef", category: "Meat" },
+];
 
+// Categories for dropdown
 export const categories = [
     "Vegetables",
     "Fruits",
